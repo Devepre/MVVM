de642dc51e66d643fd0c7ad4e0838f0bf18edd5a
Devepre <bicycle2life@gmail.com>
2019-01-27 22:11:56 +0200

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Limitation on 1/27/19.
+//  Copyright © 2019 Serhii K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
